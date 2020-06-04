9feb4c20ab58817184802d773b16a95bb0e4519c
Nathan Dardengo Bastos <nathan_dardengo@hotmail.com>
2020-06-04 20:33:21 -0300

    Aula06 Backend

---
+++ aula06/backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = express.Router();
+const UserController = require('./controllers/userController')
+
+routes.post('/users',UserController.persistUser);
+routes.get('/users',UserController.listUsers);
+routes.get('/userGit',UserController.getUserGit);
+routes.get('/gitUser2020',UserController.gitUser2020);
+
+
+//Login
+routes.post('/login/:username',UserController.login)
+module.exports = routes;
